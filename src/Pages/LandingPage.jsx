f73c49dda3cc65cb5f3500a4dbd50c97da966dfc
KroNicalKODER <mradul18varshney@gmail.com>
2025-01-17 00:17:54 +0530

    task-done

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { register, login, logout } from "../redux/authSlice";
 import Sidebar from "../Components/Sidebar";
 import MainLanding from "../Components/MainLanding";
 import LoginRegister from "../Components/LoginRegister";
 import { useTheme } from "../Contexts/ThemeContext";
 import { useSidebar } from "../Contexts/SidebarContext";
-import themeJson from "../constants/Themes.json";
 
 const LandingPage = () => {
   const { theme } = useTheme();
   const { sidebar } = useSidebar();
-  const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
   return (
     <div
